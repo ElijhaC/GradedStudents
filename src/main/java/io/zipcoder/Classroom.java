7dcd9064e6f660f4d873cd59f18a77d3686219a6
elijhac <ej9314@hotmail.com>
2022-11-30 22:23:31 -0500

    Ceated student getters and setters

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,9 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Classroom {
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores;
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,34 @@
 package io.zipcoder;
 
 public class Student {
+    String firstName;
+    String lastName;
+    int totalExams;
+    Double[] testScores;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Double[] getTestScores() {
+        return testScores;
+    }
+    public int getNumberOfExamsTaken(){
+        return totalExams;
+    }
+
+    public int getExamScores(){
+        return 0;
+    }
 }
